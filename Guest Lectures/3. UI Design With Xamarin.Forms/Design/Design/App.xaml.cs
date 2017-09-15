e3fb978e6a6f026ac7b4de53f790328c068b7e8c
Saamer Mansoor <i@saamer.me>
2017-09-15 15:42:00 -0600

    unzipped

---
+++ Guest Lectures/3. UI Design With Xamarin.Forms/Design/Design/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Design
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent ();
+
+			//MainPage = new NavigationPage (new FontsPage ());
+			MainPage = new NavigationPage(new ColorPage());
+		}
+	}
+}
---
+++ Guest Lectures/3. UI Design With Xamarin.Forms/Design/Design/ColorPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Design
+{
+	public partial class ColorPage : ContentPage
+	{
+		public ColorPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
